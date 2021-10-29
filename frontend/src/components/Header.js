a03406f28f872195901533dc713734886a6dcbad
April Hughes <ahughes0389@gmail.com>
2021-10-29 11:14:06 -0700

    added bootstrap, created header and footer components

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import {Container, Nav, Navbar, NavDropdown} from 'react-bootstrap'
+
+const Header = () => {
+    return (
+        <header>
+            <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+                <Container>
+                    <Navbar.Brand href="/">TJ Model Trains</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ms-auto">
+                        <Nav.Link href="/cart">Cart</Nav.Link>
+                        <Nav.Link href="/login">Sign In</Nav.Link>
+                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar> 
+        </header>
+    )
+}
+
+export default Header
