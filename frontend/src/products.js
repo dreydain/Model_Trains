2f3992f26f3bc6274cc0466e91496e84aed71cfb
April Hughes <ahughes0389@gmail.com>
2021-11-04 10:03:49 -0700

    added images for tj model trains and updated object key value pairs in products.js

---
+++ frontend/src/products.js
@@ -0,0 +1,76 @@
+const products = [
+  {
+    _id: '1',
+    prodNum: 'tjm1001',
+    name: 'Christmas Express',
+    image: '/images/christmas_express.jpg',
+    description:
+      'Perfect for your holiday decor, let the Christmas Express complete your Christmas Scene',
+    brand: 'TJ Model Trains',
+    category: 'trains',
+    price: 60.00,
+    countInStock: 10,
+  },
+  {
+    _id: '2',
+    prodNum: 'tjm1002',
+    name: 'Steam Engine',
+    image: '/images/steam_engine.jpg',
+    description:
+      'The Little Steam Engine that could',
+    brand: 'TJ Model Trains',
+    category: 'trains',
+    price: 50.00,
+    countInStock: 7,
+  },
+  {
+    _id: '3',
+    prodNum: 'tfy1001',
+    name: 'Red Engine',
+    image: '/images/red_engine.jpg',
+    description:
+      '1999 Red Engine',
+    brand: 'Trains For You',
+    category: 'trains',
+    price: 60.00,
+    countInStock: 5,
+  },
+  {
+    _id: '4',
+    prodNum: 'tfy2001',
+    name: 'Train Station',
+    image: '/images/train_station.jpg',
+    description:
+      'Model Train Station',
+    brand: 'Trains For You',
+    category: 'stations',
+    price: 100.00,
+    countInStock: 11,
+  },
+  {
+    _id: '5',
+    prodNum: 'tw1001',
+    name: 'Yellow Caboose',
+    image: '/images/yellow_caboose.jpg',
+    description:
+      'Little Yellow Caboose',
+    brand: 'Train World',
+    category: 'trains',
+    price: 30.00,
+    countInStock: 7,
+  },
+  {
+    _id: '6',
+    prodNum: 'tw1002',
+    name: 'Yellow Engine',
+    image: '/images/yellow_engine.jpg',
+    description:
+      'Yellow Engine',
+    brand: 'Train World',
+    category: 'trains',
+    price: 29.99,
+    countInStock: 0,
+  },
+]
+
+export default products
